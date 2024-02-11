872be4ef354dd2c57f30d65cbb574a95c3a0c801
Artur Kharisov <arturkakharisov@yandex.ru>
2024-02-11 16:00:15 +0300

    add description for all predicates

--- traditional_russian_family_with_predicates.pl
+++ traditional_russian_family_with_predicates.pl
@@ -52,47 +52,88 @@ parent(duhovlad,zlatomir).
 parent(zhdana,zdislava).
 parent(zhdana,zlatomir).
 
+% man/0
 men :- man(X), write(X), nl, fail.
 
+% woman/0
 women :- woman(X), write(X), nl, fail.
 
-%children(+X) - униф (-X) - неуниф (?X) - или то или то
+% (+X) - униф, (-X) - неуниф, (?X) - или то или то
+
+% children(+X)
+% DESCRIPTION:
+% writes all X's children
 children(X) :- parent(X, Y), write(Y), nl, fail.
 
 %children2(+X1, +X2)
 %children2(X1, X2) :- parent(X1, Y), parent(X2, Y), man(X1), woman(X2), write(Y), nl, fail.
 
-%mother(+X, +Y)
+% mother(?X, +Y)
+% DESCRIPTION:
+% checks if X is Y's mother
+% or finds Y's mother
 mother(X, Y) :- parent(X, Y), woman(X).
 
-%mother(+X)
+% mother(+X)
+% DESCRIPTION:
+% writes X's mother
 mother(X) :- parent(Y, X), woman(Y), write(Y), fail.
 
-%brother(+X, +Y)
+% brother(?X, +Y)
+% DESCRIPTION:
+% checks if X is Y's brother
+% or finds Y's brother
 brother(X, Y) :- parent(X1, X), parent(X1, Y), man(X), not(X==Y).
 
-%brothers(+X)
+% brothers(+X)
+% DESCRIPTION:
+% writes X's brothers
 brothers(X) :- setof(t, brother(Y, X), _), write(Y), fail.
 
-%b_s(+X, +Y)
+% b_s(+X, +Y)
+% DESCRIPTION:
+% checks if X is Y's brother or sister
+% or finds Y's brother or sister
 b_s(X, Y) :- parent(X1, X), parent(X1, Y), not(X==Y).
 
-%b_s(+X)
+% b_s(+X)
+% DESCRIPTION:
+% writes X's brothers and sisters
 b_s(X) :- setof(t, b_s(X, Y), _), write(Y), nl, fail.
 
-%father (+X, +Y)
+% father (+X, +Y)
+% DESCRIPTION:
+% checks if X is Y's father
+% or finds Y's father
 father(X, Y) :- parent(X, Y), man(X).
 
-%father(+X)
+% father(+X)
+% DESCRIPTION:
+% writes X's father
 father(X) :- parent(Y, X), man(Y), write(Y), fail.
 
-%grand_pa(+X, +Y)
+% grand_pa(+X, +Y)
+% DESCRIPTION:
+% chick if X grand pa for Y
 grand_pa(X, Y) :- parent(X, X1), parent(X1, Y), man(X).
 
-%grand_pas(+X)
+% grand_pas(+X)
+% DESCRIPTION:
+% writes X's grand pas
 grand_pas(X) :- grand_pa(Y, X), write(Y), nl, fail.
 
-grand_pa_and_son(X,Y) :- grand_pa(X,Y); parent(X1, X), parent(Y, X1), man(X), man(Y).
+% grand_pa_and_son(+X, +Y)
+% DESCRIPTION:
+% check if X grand pa for Y
+% or Y grand pa for X
+grand_pa_and_son(X, Y) :- grand_pa(X, Y); parent(X1, X), parent(Y, X1), man(X), man(Y).
 
-%uncle(+X, +Y)
+% uncle(+X, +Y)
+% DESCRIPTION:
+% check if X uncle for Y
 uncle(X, Y) :- parent(X1, Y), parent(X2, X1), parent(X2, X), not(X1==X), man(X).
+
+% uncles(+X)
+% DESCRIPTION:
+% writes X's uncles
+uncles(X) :- setof(t, uncle(Y, X), _), write(Y), nl, fail.
